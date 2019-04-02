e54557fef877798d8bd07ab5b99dd6aa6edd9a74
bisai96 <bisai.rosa@gmail.com>
2019-04-02 11:20:23 -0600

    reto 8-2

---
+++ reto8/pkg2/ExceptionNumero.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto8.pkg2;
+
+/**
+ *
+ * @author bisai
+ */
+public class ExceptionNumero extends Exception{
+    public ExceptionNumero(String msg);
+    super(msg);
+}
